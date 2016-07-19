3a32bc2ad45240c1e1c8f47a9975764d81d0f4fb
liwenwen <476848607@qq.com>
2016-07-19 22:44:25 +0800

    第二天，接收和回复消息

--- libs/util.js
+++ libs/util.js
@@ -5,6 +5,7 @@
 
 var fs = require('fs')
 var Promise = require('bluebird')
+var xml2js = require('xml2js')
 
 exports.readFileAsync = function(fpath, encoding){
   return new Promise(function(resole, reject){
@@ -22,3 +23,42 @@ exports.writeFileAsync = function(fpath, content){
     })
   })
 }
+
+exports.parseXMLAsync = function(xml){
+  return new Promise(function(resolve, reject){
+    xml2js.parseString(xml, {trim:true}, function(err, content){
+      if(err) reject(err)
+      else resolve(content)
+    })
+  })
+}
+
+function formatMessage(result){
+  var message = {}
+  if(typeof result === 'object'){
+    var keys = Object.keys(result)
+    for(var i = 0; i < keys.length; i++){
+      var item = result[keys[i]]
+      var key = keys[i]
+      if(!(item instanceof Array) || item.length === 0){
+        continue
+      }
+      if(item.length === 1){
+        var val = item[0]
+        if(typeof val === 'object'){
+          message[key] = formatMessage(val)
+        }else{
+          message[key] = (val || '').trim()
+        }
+      }else{
+        message[key] = []
+        for(var j = 0; j < item.length; j++){
+          message[key].push(formatMessage(item[j]))
+        }
+      }
+    }
+  }
+  return message
+}
+
+exports.formatMessage = formatMessage
--- weichat/g.js
+++ weichat/g.js
@@ -4,88 +4,19 @@
 'use strict'
 
 var sha1 = require('sha1')
-var Promise = require("bluebird")
-var request = Promise.promisify(require('request'))
-var prefix = "https://api.weixin.qq.com/cgi-bin/"
-var api = {
-  accessToken: prefix + "token?grant_type=client_credential"
-}
-
-function Wechat(opts){
-  var that = this
-  this.appID = opts.appID
-  this.appSecret = opts.appSecret
-  this.getAccessToken = opts.getAccessToken
-  this.saveAccessToken = opts.saveAccessToken
-
-  this.getAccessToken().then(function(data){
-    try {
-      data = JSON.parse(data)
-    }
-    catch(e){
-      return that.updateAccessToken(data)
-    }
-
-    if(that.isValidAccessToken(data)){
-      return Promise.resolve(data)
-    }else{
-      return that.updateAccessToken()
-    }
-  }).then(function(data){
-    that.access_token = data.access_token
-    that.expires_in = data.expires_in
-
-    that.saveAccessToken(data)
-    return Promise.resolve(data)
-  })
-}
-
-Wechat.prototype.isValidAccessToken = function(data){
-  if(!data || !data.access_token || !data.expires_in){
-    return false
-  }
-
-  var access_token = data.access_token
-  var expires_in = data.expires_in
-  var now = (new Date().getTime())
-
-  if(now < expires_in){
-    return true
-  }else{
-    return false
-  }
-
-}
-
-Wechat.prototype.updateAccessToken = function(){
-  var appID = this.appID
-  var appSecret = this.appSecret
-  var url =api.accessToken + '&appid=' + appID + '&secret=' + appSecret
-
-  return new Promise(function(resolve, reject){
-    request({url: url, json: true}).then(function(response){
-      var data = response[0]
-      console.log(data)
-      var now = (new Date().getTime())
-      var expires_in = now + (data.expires_in - 20) * 1000
-
-      data.expires_in = expires_in
-
-      resolve(data)
-
-    })
-  })
+var getReqBody = require('raw-body')
+var Wechat = require('./wechat')
+var util = require('../libs/util')
 
 
-}
 
 
 module.exports = function(opts){
   var wechat = new Wechat(opts)
 
   return function *(next){
-    console.log(this.query)
-
+    //console.log(this.query)
+    var that = this
     var token = opts.token
     var signature = this.query.signature
     var nonce = this.query.nonce
@@ -94,13 +25,48 @@ module.exports = function(opts){
     var str = [token, timestamp, nonce].sort().join('')
     var sha = sha1(str)
 
-    if(sha === signature){
-      this.body = echostr + ''
-    }else{
-      this.body = 'wrong'
+    if(this.method === "GET"){
+      if(sha === signature){
+        this.body = echostr + ''
+      }else{
+        this.body = 'wrong'
+      }
+    }else if(this.method === "POST"){
+      if(sha !== signature){
+        this.body = 'wrong'
+        return false
+      }
+      var data = yield getReqBody(this.req, {
+        "length": this.length,
+        "limit": '1mb',
+        "encoding": this.charset
+      })
+
+      var content = yield util.parseXMLAsync(data)
+      console.log(content)
+      var message = util.formatMessage(content.xml)
+      console.log(message)
+      if(message.MsgType ==='event'){
+        if(message.Event === 'subscribe'){
+          var now = new Date().getTime()
+
+          that.status = 200
+          that.type = 'application/xml'
+          that.body = '<xml>'+
+          '<ToUserName><![CDATA[' + message.FromUserName + ']]></ToUserName>'+
+          '<FromUserName><![CDATA[' + message.ToUserName + ']]></FromUserName>'+
+          '<CreateTime>' + now +'</CreateTime>'+
+          '<MsgType><![CDATA[text]]></MsgType>'+
+          '<Content><![CDATA[你好]]></Content>'+
+          '</xml>'
+
+          return
+        }
+      }
     }
 
-    this.body = this.query.echo
+
+    //this.body = this.query.echo
   }
 }
 
